f09b741d9828bf2b5ddbfe99fb9c9e6a8e836945
Peter Ďuriač <peter.duriac@morosystems.cz>
2014-11-16 00:52:48 +0100

    zmeny

--- src/main/java/cz/moro/freedom/model/World.java
+++ src/main/java/cz/moro/freedom/model/World.java
@@ -7,6 +7,8 @@ public class World {
     public static final int DEFAULT_WORLD_SIZE = 10;
     
     private final Cell[][] world;
+    private int width = DEFAULT_WORLD_SIZE;
+    private int height = DEFAULT_WORLD_SIZE;
     
     public World() {
         this(DEFAULT_WORLD_SIZE, DEFAULT_WORLD_SIZE);
@@ -14,6 +16,8 @@ public class World {
         
     public World(int width, int height) {
         world = new Cell[width][height];
+        this.height = height;
+        this.width = width;
         
         for(int x=0; x < width; x++) {
             for(int y=0; y < height; y++) {
@@ -25,5 +29,21 @@ public class World {
     public Cell getCell(int x, int y) {
         return world[x][y];
     }
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
     
 }
