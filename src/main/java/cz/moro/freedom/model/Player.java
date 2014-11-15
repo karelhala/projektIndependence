3bcabd96f8a48b9ae1dc410db620b9135ca8a961
Peter Ďuriač <peter.duriac@morosystems.cz>
2014-11-16 00:11:27 +0100

    pridany team k playerovi

--- src/main/java/cz/moro/freedom/model/Player.java
+++ src/main/java/cz/moro/freedom/model/Player.java
@@ -4,13 +4,21 @@ package cz.moro.freedom.model;
 public class Player {
 
     private final String name;
+    private final String team;
 
-    public Player(String name) {
-        this.name = name;
-    }
-    
-    public String getName() {
+    public Player(String name, String team) {
+		super();
+		this.name = name;
+		this.team = team;
+	}
+
+	public String getName() {
         return name;
     }
+
+	public String getTeam() {
+		return team;
+	}
+	
     
 }
