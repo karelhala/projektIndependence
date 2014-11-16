c136e9abbe14cade313b2d13b6ce53defd8b6831
Michal Duris <michal.duris@morosystems.cz>
2014-11-16 01:01:17 +0100

    Added set methods

--- src/main/java/cz/moro/freedom/model/Game.java
+++ src/main/java/cz/moro/freedom/model/Game.java
@@ -5,16 +5,25 @@ import java.util.List;
 
 
 public class Game {
-    
+
 
     private final World world;
     private final List<Team> teams;
-    
+
     public Game() {
 
         this.world = new World();
         this.teams = new ArrayList<>(2);
     }
-    
-    
+
+
+    public World getWorld() {
+        return world;
+    }
+
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
 }
