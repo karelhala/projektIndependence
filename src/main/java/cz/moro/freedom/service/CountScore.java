a062a6b46295b12b6884b2bc78043499dba66c5a
Peter Ďuriač <peter.duriac@morosystems.cz>
2014-11-16 03:07:11 +0100

    pocitanie pre stlpce

--- src/main/java/cz/moro/freedom/service/CountScore.java
+++ src/main/java/cz/moro/freedom/service/CountScore.java
@@ -1,81 +1,112 @@
 package cz.moro.freedom.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cz.moro.freedom.model.Cell;
+import cz.moro.freedom.model.Game;
 import cz.moro.freedom.model.Player;
+import cz.moro.freedom.model.Team;
 import cz.moro.freedom.model.World;
 
 public class CountScore {
+	private static final int LENGTH_OF_SEQUENCE = 3;
 	
 	public static class Score {
-		
-		public Long firstTeamScore;
-		public Long secondTeamScore;
-		
+
+		public Long id;
+		public Long teamScore;
+
 		/**
 		 * @param firstTeamScore
 		 * @param secondTeamScore
 		 */
-		public Score(Long firstTeamScore, Long secondTeamScore) {
-			super();
-			this.firstTeamScore = firstTeamScore;
-			this.secondTeamScore = secondTeamScore;
+		public Score(Long id, Long teamScore) {
+			this.id = id;
+			this.teamScore = teamScore;
 		}
 
-		public Long getFirstTeamScore() {
-			return firstTeamScore;
+		public Long getId() {
+			return id;
 		}
 
+		public Long getTeamScore() {
+			return teamScore;
+		}
 
-
-		public Long getSecondTeamScore() {
-			return secondTeamScore;
+		public void setTeamScore(Long teamScore) {
+			this.teamScore = teamScore;
 		}
 		
+
 	}
 
-	public static Score  makeCount(World world){
-		
-		Score totalScore = new Score(0l,0l);
-		int scoreold = 0;
-		int score = 0;
+	public static List<Score> makeCount(Game game) {
+
+		World world = game.getWorld();
+		List<Score> totalScore = initScore(game.getTeams());
+
+		Long scoreold;
+		Long score;
 		Cell cell = new Cell();
 		Player playerAcct = null;
 		Player playerOld = null;
-		
-		//prejdem hracie pole z hora dole
-		for (int x=0; x < world.getWidth(); x++){
-			scoreold = 0;
-			score = 0; 	//na zaciatku stlpca sa pocitadlo vynuluje
-			for (int y=0; y < world.getHeight(); y++){
+
+		// prejdem hracie pole z hora dole
+		for (int x = 0; x < world.getWidth(); x++) {
+			playerAcct = null;
+			playerOld = null;
+			scoreold = 0l;
+			score = 0l; // na zaciatku stlpca sa pocitadlo vynuluje
+			for (int y = 0; y < world.getHeight(); y++) {
 				cell = world.getCell(x, y);
-				
-				if (playerOld != null){
+
+				if (playerOld != null) { // kontrola zaciatku
 					playerOld = playerAcct;
 					playerAcct = cell.getPlayer();
 					score = compareCell(playerOld, playerAcct, scoreold);
-					if (score < scoreold){
-						//pridaj niekomu skore
-					}
-					else{
+					if (score < scoreold) {	// zmenil sa znak 
+						if (scoreold >= LENGTH_OF_SEQUENCE){ // ak bola postupnost vacsia ako minimalna dlzka prirad body
+							for (Score teamScore : totalScore) {
+								if(teamScore.getId() == playerOld.getTeam().getId()){
+									teamScore.setTeamScore(scoreold);
+								}
+							}
+						}
+					} else {
 						scoreold = score;
 					}
-				}
-				else{
+				} else {
 					playerAcct = cell.getPlayer();
 				}
-				
+
 			} // koniec y
 		} // koniec x
-		
-		return totalScore;		 
+
+		return totalScore;
 	}
-	
-	private static int compareCell(Player old, Player acct, int count){
-		int result = count;
-		
-		
+
+	private static List<Score> initScore(List<Team> teams) {
+		List<Score> result = new ArrayList<Score>();
+
+		for (Team team : teams) {
+			Score score = new Score(team.getId(), 0l);
+			result.add(score);
+		}
 		return result;
 	}
-	
+
+	private static Long compareCell(Player old, Player acct, Long count) {
+		Long result = count;
+		if (acct == null) {  //policko nie je obsadene
+			result = 0l; 
+		} else if (old.getTeam().equals(acct)) { //je rovnake ako predchadzajuce
+			result++;
+		} else {	// novy vyskit
+			result = 1l;
+		}
+
+		return result;
+	}
 
 }
