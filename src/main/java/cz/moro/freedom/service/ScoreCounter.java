8bbb398f043835f53a96e0c87c0fbf09a82d5b04
Peter Ďuriač <peter.duriac@morosystems.cz>
2014-11-17 05:49:26 +0100

    counter

--- src/main/java/cz/moro/freedom/service/ScoreCounter.java
+++ src/main/java/cz/moro/freedom/service/ScoreCounter.java
@@ -11,9 +11,8 @@ import cz.moro.freedom.model.World;
 
 public class ScoreCounter {
 	private static final int LENGTH_OF_SEQUENCE = 3;
-	private static List<Score> totalScore; 
+	private static List<Score> totalScore;
 
-	
 	public static class Score {
 
 		public Long id;
@@ -43,122 +42,161 @@ public class ScoreCounter {
 		public void setId(Long id) {
 			this.id = id;
 		}
-		
 
 	}
 
 	public static List<Score> getGameScore(Game game) {
 
 		World world = game.getWorld();
-		
+
 		totalScore = initScore(game.getTeams());
-		Long scoreold;
-		Cell cell = new Cell();
-		Player player = null;
 
-		// prejdem hracie pole z hora dole
-		for (int x = 0; x < world.getWidth(); x++) {
-			player = null;
-			scoreold = 0l;
-			for (int y = 0; y < world.getHeight(); y++) {
-				cell = world.getCell(x, y);
-				scoreold = getScoreFor(player,scoreold,cell);			
-				player = cell.getPlayer();				
-			} // koniec y
-		} // koniec x
-		
-		//prejde hracie pole y lava do prava
-		for (int y = 0; y < world.getHeight(); y++) {
-			player = null;
-			scoreold = 0l;
-			for (int x = 0; x < world.getWidth(); x++) {
-				cell = world.getCell(x, y);
-				scoreold = getScoreFor(player,scoreold,cell);				
-				player = cell.getPlayer();
-			} // koniec x
-		} // koniec y
+		something(world.getWidth(), world.getHeight(), false, world);
 
-		//diagony z lava do prava pod hlavnou
-		for (int y = 0; y < world.getHeight()-LENGTH_OF_SEQUENCE; y++) {
-			player = null;
-			scoreold = 0l;
-			for (int x = 0; x < world.getWidth(); x++) {
-				int newy = y+x;
-				if (newy >= world.getHeight() || newy >= x) {
-					continue;				
-				}
-				cell = world.getCell(x, y+x);
-				scoreold = getScoreFor(player,scoreold,cell);	
-				player = cell.getPlayer();
-			}
-		}
-		//diagony z lava do prava nad hlavnou
-		for (int x = 0; x < world.getWidth()-LENGTH_OF_SEQUENCE; x++) {
-			player = null;
-			scoreold = 0l;
-			for (int y = 0; y < world.getHeight(); y++) {
-				int newx = x+y;
-				if (newx >=world.getWidth() || newx >=y){
-					continue;
-				}
-				cell = world.getCell(x+y, y);
-				scoreold = getScoreFor(player,scoreold,cell);	
-				player = cell.getPlayer();
-			}
-		}
+		something(world.getHeight(), world.getWidth(), true, world);
 		
-		//diagony z prava do lava pod hlavnou
-				for (int y = world.getHeight()-1; y > 0+LENGTH_OF_SEQUENCE; y--) {
-					player = null;
-					scoreold = 0l;
-					for (int x = 0; x < world.getWidth() ; x++) {
-						int newy = y-x;
-						if (newy < 0 || x >= y ){
-							continue;
-						}
-						cell = world.getCell(x, y-x);
-						scoreold = getScoreFor(player,scoreold,cell);	
-						player = cell.getPlayer();
-					}
-				}
-				//diagony z prava do lava nad hlavnou
-				for (int x = world.getWidth()-1; x > 0+LENGTH_OF_SEQUENCE; x--) {
-					player = null;
-					scoreold = 0l;
-					for (int y =0; y < world.getHeight(); y++) {
-						int newx = x-y;
-						if (newx < 0 || y >= x){
-							continue;
-						}
-						cell = world.getCell(x-y, y);
-						scoreold = getScoreFor(player,scoreold,cell);	
-						player = cell.getPlayer();
-					}
-				}
-		
+		something2(world.getHeight(),  world);
+
+
 		return totalScore;
 	}
 
-	private static Long getScoreFor(Player player, Long oldScore, Cell cell){
+	private static void something2(int xAxis, 
+			World world){
+		Team firstTeam;
+		Team secondTeam;
+		Long counter = 1l;
+		Long counter2 = 1l;
+		
+		for(int j = 0; j < xAxis; j++){
+			
+			firstTeam = getPlayersTeamFromWorld(world, 0, j);
+			
+			for(int i = 0; i <=j; i++){
+				secondTeam = getPlayersTeamFromWorld(world, i, (j-i));
+				
+				if (firstTeam != null && firstTeam.equals(secondTeam)) {
+
+					if ((j-i) > 0 ) {
+						counter++;
+					} else {
+						setScoreToTeam(firstTeam, counter);
+						counter = 1l;
+					}
+
+				} else if (counter >= LENGTH_OF_SEQUENCE) {
+
+					setScoreToTeam(firstTeam, counter);
+					counter = 1l;
+				}
+
+				firstTeam = secondTeam;
+			}
+		}
+	}
+	private static void something(int xAxis, int yAxis, boolean inversedAxis,
+			World world) {
+
+		Team firstTeam;
+		Team secondTeam;
+		Long counter = 1l;
+
+		int x;
+		int y;
+
+		for (int i = 0; i < xAxis; i++) {
+
+			if (inversedAxis) {
+				x = 0;
+				y = i;
+			} else {
+				x = i;
+				y = 0;
+			}
+
+			firstTeam = getPlayersTeamFromWorld(world, x, y);
+
+			for (int j = 1; j < yAxis; j++) {
+
+				if (inversedAxis) {
+					x = j;
+				} else {
+					y = j;
+				}
+
+				secondTeam = getPlayersTeamFromWorld(world, x, y);
+
+				if (firstTeam != null && firstTeam.equals(secondTeam)) {
+
+					if (j < world.getHeight()) {
+						counter++;
+					} else {
+						setScoreToTeam(firstTeam, counter);
+						counter = 1l;
+					}
+
+				} else if (counter >= LENGTH_OF_SEQUENCE) {
+
+					setScoreToTeam(firstTeam, counter);
+					counter = 1l;
+				}
+
+				firstTeam = secondTeam;
+			}
+		}
+
+	}
+
+	private static Team getPlayersTeamFromWorld(World world, int x, int y) {
+
+		return world.getCell(x, y).getPlayer() != null ? world.getCell(x, y)
+				.getPlayer().getTeam() : null;
+	}
+
+	private static void setScoreToTeam(Team team, Long counter) {
+
+		for (Score score : totalScore) {
+
+			if (score.getId().equals(team.getId())) {
+
+				score.setTeamScore(score.getTeamScore()
+						+ (counter - LENGTH_OF_SEQUENCE + 1));
+			}
+		}
+	}
+
+	private static Long getScoreFor(Player player, Player playerold,
+			Long oldScore, Cell cell) {
 		Long score = 0l;
 		if (player != null) { // kontrola zaciatku
-			 score = getNewScore(player,cell.getPlayer(),oldScore);
-			if (score < oldScore) {	// zmenil sa znak 
-				if (oldScore >= LENGTH_OF_SEQUENCE){ // ak bola postupnost vacsia ako minimalna dlzka prirad body
+			score = getNewScore(playerold, player, oldScore);
+			if (score < oldScore) { // zmenil sa znak
+				if (oldScore >= LENGTH_OF_SEQUENCE) { // ak bola postupnost
+														// vacsia ako minimalna
+														// dlzka prirad body
 					for (Score teamScore : totalScore) {
-						if(teamScore.getId() == player.getTeam().getId()){
-							teamScore.setTeamScore(oldScore);
+						if (teamScore.getId() == player.getTeam().getId()) {
+							teamScore.setTeamScore(teamScore.getTeamScore()
+									+ (oldScore - LENGTH_OF_SEQUENCE + 1));
 						}
-					}								
 					}
-				} 
-			} 
+				}
+			}
+		} else if (oldScore >= LENGTH_OF_SEQUENCE) {
+			for (Score teamScore : totalScore) {
+				if (teamScore.getId() == playerold.getTeam().getId()) {
+					teamScore.setTeamScore(teamScore.getTeamScore()
+							+ (oldScore - LENGTH_OF_SEQUENCE + 1));
+				}
+			}
+		}
 		return score;
 	}
-	private static Long getNewScore(Player old, Player acct, Long scoreOld){			
+
+	private static Long getNewScore(Player old, Player acct, Long scoreOld) {
 		return compareCell(old, acct, scoreOld);
 	}
-	
+
 	private static List<Score> initScore(List<Team> teams) {
 		List<Score> result = new ArrayList<>();
 
@@ -171,11 +209,14 @@ public class ScoreCounter {
 
 	private static Long compareCell(Player old, Player acct, Long count) {
 		Long result = count;
-		if (acct == null) {  //policko nie je obsadene
-			result = 0l; 
-		} else if (old.getTeam().equals(acct)) { //je rovnake ako predchadzajuce
+		if (acct == null) { // policko nie je obsadene
+			result = 0l;
+		} else if (old == null) {
+			return 1l;
+		} else if (old.getTeam().equals(acct.getTeam())) { // je rovnake ako
+															// predchadzajuce
 			result++;
-		} else {	// novy vyskit
+		} else { // novy vyskit
 			result = 1l;
 		}
 
